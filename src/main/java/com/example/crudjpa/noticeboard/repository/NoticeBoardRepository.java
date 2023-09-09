fecd507ce46fa993a3476f1a2721fa1541b6e399
wook <mystyle730@gmail.com>
2023-09-09 17:00:49 +0900

    게시판 테스트코드 추가 및 게시판레포지토리 수정

--- src/main/java/com/example/crudjpa/noticeboard/repository/NoticeBoardRepository.java
+++ src/main/java/com/example/crudjpa/noticeboard/repository/NoticeBoardRepository.java
@@ -14,11 +14,11 @@ public interface NoticeBoardRepository extends JpaRepository<NoticeBoardEntity, 
     //게시물 상세 조회
 
     //제목 전체 리스트조회
-    List<NoticeBoardEntity> findAllByBoardTitle(String boardTitle);
+    List<NoticeBoardEntity> findAllByBoardTitleContaining(String boardTitle);
     //내용 전체리스트조회
-    List<NoticeBoardEntity> findAllByBoardCn(String boardCn);
+    List<NoticeBoardEntity> findAllByBoardCnContaining(String boardCn);
     //제목+내용 전체리스트조회
-    List<NoticeBoardEntity> findAllByBoardTitleOrBoardCn(String boardTitle, String boardCn);
+    List<NoticeBoardEntity> findAllByBoardTitleContainingOrBoardCnContaining(String boardTitle, String boardCn);
     //리스트 좋아요 정렬
     List<NoticeBoardEntity> findAllByOrderByBoardLikeDesc();
     //리스트 싫어요 정렬
--- src/test/java/com/example/crudjpa/domain/noticeboard/NoticeBoardRepositoryTest.java
+++ src/test/java/com/example/crudjpa/domain/noticeboard/NoticeBoardRepositoryTest.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,58 +34,13 @@ public class NoticeBoardRepositoryTest {
 
     @Before
     public void setUp() {
-        String boardTitle = "제목";
-        String boardCn = "내용";
-        String writer = "mystyle730gmial.com";
-        LocalDateTime nowDate = LocalDateTime.now();
         //ManyToOne <-> OneToMany 양방향관계에서는 One쪽을 먼저 생성
         //Many을 그다음에 생성한다.
 
-//        NoticeBoardEntity noticeBoardEntity = NoticeBoardEntity.builder()
-//                .boardTitle(boardTitle)
-//                .boardCn(boardCn)
-//                .boardViews(12)
-//                .boardLike(10)
-//                .boardDontLike(10)
-//                .boardFstRegNm(writer)
-//                .boardFstRegDt(nowDate)
-//                .boardUptRegNm(writer)
-//                .boardUptRegDt(nowDate)
-//                .rowStatCd("C")
-//                .build();
-//
-//        noticeBoardRepository.save(noticeBoardEntity);
-//
-//        IntStream.rangeClosed(0,10).forEach(i -> {
-//            CommentEntity commentEntity = CommentEntity.builder()
-//                    .commentCn("댓글내용" + i)
-//                    .commentLike(i)
-//                    .commentDontLike(i)
-//                    .commentFstRegNm(writer + i)
-//                    .commentFstRegDt(nowDate)
-//                    .commentUptRegNm(writer + i)
-//                    .commentUptRegDt(nowDate)
-//                    .rowStatCd("C")
-//                    .noticeBoardEntity(noticeBoardEntity)
-//                    .build();
-//            commentRepository.save(commentEntity);
-//        });
-    }
-
-    @After
-    public void cleanup() {
-        //반대로 삭제할때는 Many쪽을 먼저 삭제 One쪽을 나중에 삭제한다
-        commentRepository.deleteAll();
-        noticeBoardRepository.deleteAll();
-    }
-
-    @Test
-    @DisplayName("전체게시판리스트조회")
-    public void findByAllBoardTest() {
         //when
         String boardTitle = "제목";
         String boardCn = "내용";
-        String writer = "mystyle730gmial.com";
+        String writer = "mystyle730gmail.com";
         LocalDateTime nowDate = LocalDateTime.now();
 
         IntStream.rangeClosed(0, 10).forEach(i-> {
@@ -118,6 +74,23 @@ public class NoticeBoardRepositoryTest {
                 commentRepository.save(commentEntity);
             });
         });
+    }
+
+    @After
+    public void cleanup() {
+        //반대로 삭제할때는 Many쪽을 먼저 삭제 One쪽을 나중에 삭제한다
+        commentRepository.deleteAll();
+        noticeBoardRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("전체게시판리스트조회")
+    @Transactional
+    public void findByAllBoardTest() {
+        //when
+        String boardTitle = "제목";
+        String boardCn = "내용";
+        String writer = "mystyle730gmail.com";
 
         //given
         List<NoticeBoardEntity> noticeBoardEntityList = noticeBoardRepository.findAllByOrderByBoardFstRegDtDesc();
@@ -127,56 +100,31 @@ public class NoticeBoardRepositoryTest {
             NoticeBoardEntity noticeBoardEntity = noticeBoardEntityList.get(i);
             assertThat(noticeBoardEntity.getBoardTitle()).isEqualTo(boardTitle + i);
             assertThat(noticeBoardEntity.getBoardCn()).isEqualTo(boardCn + i);
+            assertThat(noticeBoardEntity.getBoardFstRegNm()).isEqualTo(writer+i);
+            assertThat(noticeBoardEntity.getBoardUptRegNm()).isEqualTo(writer+i);
         });
     }
 
     @Test
     @DisplayName("게시판 상세조회")
+    @Transactional
     public void findByBoardIdTest() {
-        String boardTitle = "제목";
-        String boardCn = "내용";
-        String writer = "mystyle730gmial.com";
-        LocalDateTime nowDate = LocalDateTime.now();
-
-        NoticeBoardEntity noticeBoardEntitySaveData = NoticeBoardEntity.builder()
-                .boardTitle(boardTitle)
-                .boardCn(boardCn)
-                .boardViews(12)
-                .boardLike(10)
-                .boardDontLike(10)
-                .boardFstRegNm(writer)
-                .boardFstRegDt(nowDate)
-                .boardUptRegNm(writer)
-                .boardUptRegDt(nowDate)
-                .rowStatCd("C")
-                .build();
-
-        noticeBoardRepository.save(noticeBoardEntitySaveData);
-
-        IntStream.rangeClosed(0,10).forEach(i -> {
-            CommentEntity commentEntity = CommentEntity.builder()
-                    .commentCn("댓글내용" + i)
-                    .commentLike(i)
-                    .commentDontLike(i)
-                    .commentFstRegNm(writer + i)
-                    .commentFstRegDt(nowDate)
-                    .commentUptRegNm(writer + i)
-                    .commentUptRegDt(nowDate)
-                    .rowStatCd("C")
-                    .noticeBoardEntity(noticeBoardEntitySaveData)
-                    .build();
-            commentRepository.save(commentEntity);
-        });
+        //when
+        String boardTitle = "제목0";
+        String boardCn = "내용0";
+        String writer = "mystyle730gmail.com";
 
         //given
         Optional<NoticeBoardEntity> noticeBoardEntity = noticeBoardRepository.findByBoardId(1L);
 
-        //optional 사용해서 객체를 사용
+        //optional 사용해서 객체를 꺼내온다.
         if(noticeBoardEntity.isPresent()) {
             NoticeBoardEntity noticeBoardEntity1 = noticeBoardEntity.get();
             //then
             assertThat(noticeBoardEntity1.getBoardTitle()).isEqualTo(boardTitle);
             assertThat(noticeBoardEntity1.getBoardCn()).isEqualTo(boardCn);
+            assertThat(noticeBoardEntity1.getBoardFstRegNm()).isEqualTo(writer+ "0");
+            assertThat(noticeBoardEntity1.getBoardUptRegNm()).isEqualTo(writer + "0");
             List<CommentEntity> commentEntityList1 = commentRepository.findAllByNoticeBoardEntity(noticeBoardEntity1);
 
             IntStream.rangeClosed(0,10).forEach(i -> {
@@ -184,12 +132,62 @@ public class NoticeBoardRepositoryTest {
                 assertThat(commentEntityList1.get(i).getCommentLike()).isEqualTo(i);
                 assertThat(commentEntityList1.get(i).getCommentDontLike()).isEqualTo(i);
                 assertThat(commentEntityList1.get(i).getCommentFstRegNm()).isEqualTo(writer + i);
-                assertThat(commentEntityList1.get(i).getCommentFstRegDt()).isEqualTo(nowDate);
+//                assertThat(commentEntityList1.get(i).getCommentFstRegDt()).isEqualTo(nowDate);
                 assertThat(commentEntityList1.get(i).getCommentUptRegNm()).isEqualTo(writer+i);
-                assertThat(commentEntityList1.get(i).getCommentUptRegDt()).isEqualTo(nowDate);
+//                assertThat(commentEntityList1.get(i).getCommentUptRegDt()).isEqualTo(nowDate);
                 assertThat(commentEntityList1.get(i).getRowStatCd()).isEqualTo("C");
-//            assertThat(commentEntityList1.get(i).getNoticeBoardEntity()).isEqualTo(noticeBoardEntity); //이유확인
+                assertThat(commentEntityList1.get(i).getNoticeBoardEntity()).isEqualTo(noticeBoardEntity1);
             });
         }
+    }
+
+    @Test
+    @DisplayName("전체 게시판 제목으로 조회")
+    @Transactional
+    public void findAllByBoardTitleTest() {
+        //when
+        String boardTitle = "제목";
+        String boardCn = "내용";
+        String writer = "mystyle730gmail.com";
+
+        //given
+        List<NoticeBoardEntity> noticeBoardEntityList1 = noticeBoardRepository.findAllByBoardTitleContaining("제");
+        List<NoticeBoardEntity> noticeBoardEntityList2 = noticeBoardRepository.findAllByBoardTitleContaining("목");
+        List<NoticeBoardEntity> noticeBoardEntityList3 = noticeBoardRepository.findAllByBoardTitleContaining("없음");
+
+        //then
+        IntStream.rangeClosed(0,10).forEach(i-> {
+            NoticeBoardEntity noticeBoardEntity = noticeBoardEntityList1.get(i);
+            assertThat(noticeBoardEntity.getBoardTitle()).isEqualTo(boardTitle + i);
+            assertThat(noticeBoardEntity.getBoardCn()).isEqualTo(boardCn + i);
+            assertThat(noticeBoardEntity.getBoardFstRegNm()).isEqualTo(writer + i);
+            assertThat(noticeBoardEntity.getBoardUptRegNm()).isEqualTo(writer + i);
+        });
+        IntStream.rangeClosed(0,10).forEach(i-> {
+            NoticeBoardEntity noticeBoardEntity = noticeBoardEntityList2.get(i);
+            assertThat(noticeBoardEntity.getBoardTitle()).isEqualTo(boardTitle + i);
+            assertThat(noticeBoardEntity.getBoardCn()).isEqualTo(boardCn + i);
+            assertThat(noticeBoardEntity.getBoardFstRegNm()).isEqualTo(writer + i);
+            assertThat(noticeBoardEntity.getBoardUptRegNm()).isEqualTo(writer + i);
+        });
+        //검색안된경우 테스트
+        assertThat(noticeBoardEntityList3.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("전체 게시판 좋아요순 정렬 조회")
+    @Transactional
+    public void findAllByOrderByBoardLikeDescTest() {
+        //when
+        int[] arr = {10,9,8,7,6,5,4,3,2,1,0};
+
+        //given
+        List<NoticeBoardEntity> noticeBoardEntityList = noticeBoardRepository.findAllByOrderByBoardLikeDesc();
+
+        //then
+        IntStream.rangeClosed(0,10).forEach(i-> {
+           NoticeBoardEntity noticeBoardEntity = noticeBoardEntityList.get(i);
+           assertThat(noticeBoardEntity.getBoardLike()).isEqualTo(arr[i]);
+        });
     }
 }
