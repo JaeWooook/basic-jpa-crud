99b46eb04bfbb7c256605e4bc751b47f11eda586
wook <mystyle730@gmail.com>
2023-11-01 18:22:56 +0900

    게시판 상세페이지 이동, 등록, 수정, 읽기 컨트롤러 및 화면개발

--- src/main/java/com/example/crudjpa/noticeboard/controller/NoticeBoardController.java
+++ src/main/java/com/example/crudjpa/noticeboard/controller/NoticeBoardController.java
@@ -1,7 +1,9 @@
 package com.example.crudjpa.noticeboard.controller;
 
+import com.example.crudjpa.noticeboard.dto.NoticeBoardDtlDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,5 +21,22 @@ public class NoticeBoardController {
     public String moveNoticeBoardListPage() {
 
         return "views/noticeboard/NoticeBoardList";
+    }
+
+    /**
+     * 게시판 상세 페이지 이동
+     * @return
+     */
+    @GetMapping("/noticeboard/detail")
+    public String moveNoticeBoardDetailPage(NoticeBoardDtlDTO.Request noticeBoardDtlDTO, Model model) {
+        String pageFlag = noticeBoardDtlDTO.getBoardFlag();
+
+        //crete
+
+        //read
+
+        //update
+        model.addAttribute("pageFlag", pageFlag);
+        return "views/noticeboard/NoticeBoardDtl";
     }
 }
